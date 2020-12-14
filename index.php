7559e6128332a6e033bb242639c88972aaf9ac2b
Maichol <msibiriu@gmail.com>
2020-12-14 19:28:58 +0100

    esercizio completato.

--- index.php
+++ index.php
@@ -7,7 +7,36 @@
 </head>
 <body>
     
-    <h1>ciaooooooo</h1>
+    <h1>CLASSE 19</h1>
+
+    <?php
+      
+    // paragrafo
+    $text = 'corso boolean per sviluppatori web ,nel corso boolean si studia molto.';
+     
+    //  lunghezza paragrafo
+    $length = strlen($text);
+
+    //  richiamo parola da url
+    $badword = $_GET['badword'];
+    
+    //  paragrafo censurato
+    $censorship = str_replace('boolean' , '***' , $text);
+
+
+    ?>
+
+    <h1>testo :</h1>
+    <p> <?php echo $text; ?> </p>
+
+    <h1>la lunghezza del testo e' :</h1>
+    <p> <?php echo $length; ?> </p>
+
+    <h1>La parola nascosta e' :</h1>
+    <p> <?php echo $badword; ?> </p>
+
+    <h1>testo censurato :</h1>
+    <p> <?php echo $censorship; ?> </p>
 
 
      
